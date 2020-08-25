77f64ce05d3c3181bd85d0c0555f4d5d2ec6be74
beomju <20171643.info@edu.hanbat.ac.kr>
2020-08-25 17:25:03 +0900

    update config.json -> config.js

---
+++ config/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+    development: {
+      username: "beomju",
+      password: process.env.SEQUELIZE_PASSWORD,
+      database: "capstone",
+      host: "127.0.0.1",
+      dialect: "postgres",
+    }
+}
+
+
