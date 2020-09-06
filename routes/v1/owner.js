78c88182353caca14803e5831d105af1d57e6b93
beomju <20171643.info@edu.hanbat.ac.kr>
2020-09-06 23:34:40 +0900

    update 'delete-owner'

--- routes/v1/owner.js
+++ routes/v1/owner.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const Owner = require('../../models');
+const {Owner} = require('../../models');
 
-router.get('/ownerInfo/:id', async (req, res, next) => {
+router.get('/ownerInfo/:id', (req, res, next) => {
     Owner.findOne({
         where: {owner_id: req.params.id},
     })
@@ -15,6 +15,24 @@ router.get('/ownerInfo/:id', async (req, res, next) => {
             res.status(500).message('Internal Server Error');
             next(error);
         })
-})
+});
+
+
+router.delete('/delete_owner/:id', async (req, res, next) => {
+
+    console.log(req.params.id);
+    try {
+        const owner = await Owner.findOne({where: {owner_id: req.params.id}});
+        if (!owner) {
+            return res.status(409).send('해당 아이디가 없습니다.');
+        }
+        await Owner.destroy({where: {owner_id: req.params.id}});
+        res.status(200).send('해당 계정이 삭제되었습니다.');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+        next(error);
+    }
+});
 
 module.exports = router;
