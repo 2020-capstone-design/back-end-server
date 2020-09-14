d1ab3b236e83dd33fd6d3c744e2100329bea838b
beomju <20171643.info@edu.hanbat.ac.kr>
2020-09-14 17:50:54 +0900

    delete garbage code

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const morgan = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
-
-const indexRouter = require('./routes/v1');
+;
 const restaurantRouter = require('./routes/v1/restaurant');
 const menuRouter = require('./routes/v1/menu');
 const authRouter = require('./routes/v1/auth');
@@ -15,22 +14,30 @@ const { sequelize } = require('./models');
 
 const app = express();
 
-sequelize.sync();
+sequelize.sync({ force: false})
+    .then(() => {
+      console.log('데이터베이스연결 성공');
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 app.set('port', process.env.PORT || 3000);
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+} else {
+  app.use(morgan('dev'));
+}
 app.use(cors());
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/img', express.static(path.join(__dirname, 'uploads')));
 
-app.use('/v1', indexRouter);
 app.use('/v1/restaurant', restaurantRouter);
 app.use('/v1/menu', menuRouter);
 app.use('/v1/auth', authRouter)
